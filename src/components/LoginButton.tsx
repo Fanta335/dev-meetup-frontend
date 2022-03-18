ebb81af6ff8ad49fa3bf539e3a7ef10d662e8d86
Fanta335 <fantayusuke5210@gmail.com>
2022-03-18 14:59:36 +0900

    Add authentication button

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,11 +1,15 @@
 import { VFC } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-
+import { Button } from "@mui/material";
 
 const LoginButton: VFC = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Log in</button>;
+  return (
+    <Button variant="contained" onClick={() => loginWithRedirect()}>
+      Log in
+    </Button>
+  );
 };
 
 export default LoginButton;
--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -1,10 +1,15 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { Button } from "@mui/material";
 import { VFC } from "react";
 
 const LogoutButton: VFC = () => {
   const { logout } = useAuth0();
 
-  return <button onClick={() => logout({ returnTo: window.location.origin })}>Log out</button>;
+  return (
+    <Button variant="contained" onClick={() => logout({ returnTo: window.location.origin })}>
+      Log out
+    </Button>
+  );
 };
 
 export default LogoutButton;
