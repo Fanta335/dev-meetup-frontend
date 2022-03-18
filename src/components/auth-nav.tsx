1d651c20766959084a3a5847d4d2f68edd6e0197
Fanta335 <fantayusuke5210@gmail.com>
2022-03-18 14:59:54 +0900

    Add navbar

---
+++ src/components/auth-nav.tsx
@@ -0,0 +1,8 @@
+import { VFC } from "react";
+import AuthenticationButton from "./AuthenticationButton";
+
+const AuthNav: VFC = () => {
+  return <AuthenticationButton />;
+};
+
+export default AuthNav;
