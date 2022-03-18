df1607d3f4535ddcff46832950888e3b4375cbe7
Fanta335 <fantayusuke5210@gmail.com>
2022-03-18 17:05:07 +0900

    #4 Add external api component

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import React, { VFC } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import RequireAuth from "./components/auth/requireAuth";
-import Home from "./components/Home";
+import Home from "./pages/Home";
 import NavBar from "./components/Navbar";
-import Profile from "./components/Profile";
+import Profile from "./pages/Profile";
+import ExternalApi from "./pages/externalApi";
 
 const App: VFC = () => {
   const { isLoading } = useAuth0();
@@ -20,6 +21,7 @@ const App: VFC = () => {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/profile" element={<RequireAuth><Profile /></RequireAuth>}></Route>
+        <Route path='/external-api' element={<ExternalApi />}></Route>
       </Routes>
     </div>
   );
