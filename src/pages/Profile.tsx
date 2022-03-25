cb9ffcba0ddcbfcec920bd10c736f53ab5060c18
Fanta335 <fantayusuke5210@gmail.com>
2022-03-25 17:46:27 +0900

    #6 Add getUserMetadata functionality

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,38 +1,27 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { VFC } from "react";
+import { useEffect, useState, VFC } from "react";
+import { MysqlUser } from "../types";
 
 const Profile: VFC = () => {
-  const { user, isAuthenticated, isLoading } = useAuth0();
-  // const [userMetadata, setUserMetadata] = useState(null);
+  const { user, isAuthenticated, isLoading, getIdTokenClaims } = useAuth0();
+  const [mysqlUser, setMysqlUser] = useState<MysqlUser>();
+  const namespace = process.env.REACT_APP_METADATA_NAMESPACE || "https://dev-meetup-backend.com/mysqlUser";
 
-  // useEffect(() => {
-  //   const getUserMetadata = async () => {
-  //     const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+  useEffect(() => {
+    const getUserMetadata = async () => {
+      try {
+        const claims = await getIdTokenClaims();
+        if (claims !== undefined) {
+          const userData: MysqlUser = claims[namespace + "/mysqlUser"];
+          setMysqlUser(userData);
+        }
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
 
-  //     try {
-  //       const accessToken = await getAccessTokenSilently({
-  //         audience: `https://${domain}/api/v2/`,
-  //         scope: "read:current_user",
-  //       });
-
-  //       const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user?.sub}`;
-
-  //       const metadataResponse = await fetch(userDetailsByIdUrl, {
-  //         headers: {
-  //           Authorization: `Bearer ${accessToken}`,
-  //         },
-  //       });
-
-  //       const { user_metadata } = await metadataResponse.json();
-
-  //       setUserMetadata(user_metadata);
-  //     } catch (e) {
-  //       console.log(e.messsage);
-  //     }
-  //   };
-
-  //   getUserMetadata();
-  // }, [getAccessTokenSilently, user?.sub]);
+    getUserMetadata();
+  }, [getIdTokenClaims, namespace]);
 
   if (isLoading) {
     return <div>Loading ...</div>;
@@ -44,7 +33,13 @@ const Profile: VFC = () => {
       <h2>{user?.name}</h2>
       <p>{user?.email}</p>
       <h3>User Metadata</h3>
-      {/* {userMetadata ? <pre>{JSON.stringify(userMetadata, null, 2)}</pre> : "No user metadata defined"} */}
+      {mysqlUser ? <pre>{JSON.stringify(mysqlUser, null, 2)}</pre> : "No user metadata defined"}
+      {mysqlUser ? <h4>Id: {mysqlUser.id}</h4> : "no user meta"}
+      {mysqlUser ? <h4>Name: {mysqlUser.name}</h4> : "no user meta"}
+      {mysqlUser ? <h4>email: {mysqlUser.email}</h4> : "no user meta"}
+      {mysqlUser ? <h4>sub id: {mysqlUser.subId}</h4> : "no user meta"}
+      {mysqlUser ? <h4>createdAt: {mysqlUser.createdAt}</h4> : "no user meta"}
+      {mysqlUser ? <h4>updatedAt: {mysqlUser.updatedAt}</h4> : "no user meta"}
     </div>
   ) : (
     <h2>Not authenticated!</h2>
