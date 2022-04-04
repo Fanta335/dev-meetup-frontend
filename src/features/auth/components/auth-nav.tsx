19842d27904c1788d29813d7aebaf8a142d65ac7
Fanta335 <fantayusuke5210@gmail.com>
2022-04-04 15:58:21 +0900

    Remove auth components

--- src/features/auth/components/auth-nav.tsx
+++
@@ -1,8 +0,0 @@
-import { VFC } from "react";
-import AuthenticationButton from "../../../components/auth/AuthenticationButton";
-
-const AuthNav: VFC = () => {
-  return <AuthenticationButton />;
-};
-
-export default AuthNav;
