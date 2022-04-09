bab6ecd3dfce579bd7144ef3a7f1e3039397f496
Fanta335 <fantayusuke5210@gmail.com>
2022-04-09 12:40:52 +0900

    Rename Dashboard to Home

--- src/features/misc/Home.tsx
+++ src/features/misc/Home.tsx
@@ -1,11 +1,11 @@
 import { useAuth0 } from "@auth0/auth0-react";
 
-export const Dashboard = () => {
+export const Home = () => {
   const { user } = useAuth0();
 
   return (
     <div>
-      <h1>Dashboard page.</h1>
+      <h1>home page.</h1>
       <img src={user?.picture} alt={user?.name} />
       <h2>{user?.name}</h2>
       <p>{user?.email}</p>
