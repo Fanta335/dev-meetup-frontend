736079b817b610327d47c0c92acdd4d962eb4078
Fanta335 <fantayusuke5210@gmail.com>
2022-04-26 18:13:59 +0900

    Add confirm deletion dialog

---
+++ src/features/message/components/ConfirmDeletionDialog.tsx
@@ -0,0 +1,32 @@
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import { VFC } from "react";
+
+type Props = {
+  open: boolean;
+  handleClose: () => void;
+  handleDelelte: () => void;
+};
+
+export const ConfirmDeletionDialog: VFC<Props> = ({ open, handleClose, handleDelelte }) => {
+  return (
+    <div>
+      <Dialog open={open} onClose={handleClose} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
+        <DialogTitle id="alert-dialog-title">{"メッセージを削除"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">メッセージを削除します。よろしいですか？</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} autoFocus>
+            キャンセル
+          </Button>
+          <Button onClick={handleDelelte} color="error" variant="contained">削除</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
--- src/features/message/components/DeleteMessageButton.tsx
+++ src/features/message/components/DeleteMessageButton.tsx
@@ -1,9 +1,10 @@
 import { IconButton, Tooltip } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
-import { VFC } from "react";
+import { useState, VFC } from "react";
 import { useAppDispatch, useAppSelector } from "../../../stores/hooks";
 import { messageActions } from "../messageSlice";
 import { selectCurrentRoom } from "../../room/roomSlice";
+import { ConfirmDeletionDialog } from "./ConfirmDeletionDialog";
 
 type Props = {
   messageId: number;
@@ -12,15 +13,30 @@ type Props = {
 export const DeleteMessageButton: VFC<Props> = ({ messageId }) => {
   const dispatch = useAppDispatch();
   const currentRoom = useAppSelector(selectCurrentRoom);
-  const handleClick = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleDelete = () => {
     dispatch(messageActions.removeMessage({ roomId: currentRoom.id.toString(), messageId: messageId }));
     console.log("remove message id: ", messageId);
+    setOpen(false);
   };
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <Tooltip title="削除する" placement="top" arrow>
-      <IconButton onClick={handleClick}>
-        <DeleteIcon />
-      </IconButton>
-    </Tooltip>
+    <>
+      <Tooltip title="削除する" placement="top" arrow>
+        <IconButton onClick={handleClickOpen}>
+          <DeleteIcon />
+        </IconButton>
+      </Tooltip>
+      <ConfirmDeletionDialog open={open} handleClose={handleClose} handleDelelte={handleDelete} />
+    </>
   );
 };
