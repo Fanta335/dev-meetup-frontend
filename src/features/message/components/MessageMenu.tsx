80a73a8a8decf07c659296a88bfdef3bc3b23a00
Fanta335 <fantayusuke5210@gmail.com>
2022-04-26 20:26:04 +0900

    Improve messageMenu: shows edit & delete button if only current user is author the message

--- src/features/message/components/MessageMenu.tsx
+++ src/features/message/components/MessageMenu.tsx
@@ -1,7 +1,8 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { ButtonGroup } from "@mui/material";
 import { VFC } from "react";
 import { useAppSelector } from "../../../stores/hooks";
-import { selectMessageEdit } from "../messageSlice";
+import { selectCurrentMessages, selectMessageEdit } from "../messageSlice";
 import { DeleteMessageButton } from "./DeleteMessageButton";
 import { EditMessageButton } from "./EditMessageButton";
 import { ReplyButton } from "./ReplyButton";
@@ -15,6 +16,18 @@ export const MessageMenu: VFC<Props> = ({ messageId }) => {
   const messageEdit = useAppSelector(selectMessageEdit);
   const isEditing = messageEdit.isEditing && messageId === messageEdit.messageId;
 
+  // Check if current user is the author of this message.
+  // Get current user id from auth0 context.
+  const { user } = useAuth0();
+  const claim = process.env.REACT_APP_API_NAMESPACE + "/mysqlUser";
+  const currentUserId: number = user?.[claim].id;
+
+  // Get author id.
+  const currentMessages = useAppSelector(selectCurrentMessages);
+  const authorId = currentMessages.byIds[messageId].authorId;
+
+  const isOwnMessage = authorId === currentUserId;
+
   return (
     <ButtonGroup size="small" aria-label="outlined primary button group" sx={{ px: 1 }}>
       {isEditing ? (
@@ -22,8 +35,8 @@ export const MessageMenu: VFC<Props> = ({ messageId }) => {
       ) : (
         <div>
           <ReplyButton />
-          <EditMessageButton messageId={messageId} />
-          <DeleteMessageButton messageId={messageId} />
+          {isOwnMessage && <EditMessageButton messageId={messageId} />}
+          {isOwnMessage && <DeleteMessageButton messageId={messageId} />}
         </div>
       )}
     </ButtonGroup>
