b351b4bf70601d50c5c3c8f87037f890a9319344
Fanta335 <fantayusuke5210@gmail.com>
2022-05-16 16:17:39 +0900

    Add image preview feature

--- src/features/misc/components/UploadAndDisplayImage.tsx
+++ src/features/misc/components/UploadAndDisplayImage.tsx
@@ -1,3 +1,6 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import { Button } from "@mui/material";
+import axios from "axios";
 import { useEffect, useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 
@@ -9,6 +12,7 @@ export const UploadAndDisplayImage = () => {
   const { register, handleSubmit } = useForm<FormInput>();
   const [selectedFile, setSelectedFile] = useState<File>();
   const [preview, setPreview] = useState<string>();
+  const { getAccessTokenSilently } = useAuth0();
 
   useEffect(() => {
     if (!selectedFile) {
@@ -29,15 +33,33 @@ export const UploadAndDisplayImage = () => {
     setSelectedFile(e.target.files[0]);
   };
 
-  const onSubmit: SubmitHandler<FormInput> = (data) => {
-    console.log("uploaded data: ", data);
+  const onSubmit: SubmitHandler<FormInput> = async (data) => {
+    if (!selectedFile) {
+      return;
+    }
+
+    console.log("uploaded data: ", data.image[0]);
+    const formData = new FormData();
+    formData.append("file", selectedFile);
+
+    const token = await getAccessTokenSilently();
+
+    const res = await axios.post(`${process.env.REACT_APP_API_URL}/users/avatar`, formData, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    console.log("post result: ", res.data);
   };
 
   return (
     <>
       <form onSubmit={handleSubmit(onSubmit)}>
         <input {...register("image")} required type="file" onChange={onSelectFile} />
-        <button>Submit</button>
+        <Button variant="contained" color="success" type="submit">
+          保存する
+        </Button>
       </form>
       <p>image preview</p>
       {preview ? <img src={preview} alt="preview" style={{ height: "200px" }} /> : <p>no image</p>}
