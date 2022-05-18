fdd196eee1cca238a498997931286d3d0a80671f
Fanta335 <fantayusuke5210@gmail.com>
2022-05-18 15:21:03 +0900

    Add image

--- src/features/misc/Landing.tsx
+++ src/features/misc/Landing.tsx
@@ -1,13 +1,12 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { Button, Typography } from "@mui/material";
+import { Button, Grid, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { Loading } from "../../components/Loading";
-import LogoutButton from "../../components/auth/LogoutButton";
+import { ReactComponent as GoodTeam } from "../../assets/good_team.svg";
 
 export const Landing = () => {
-  const { isLoading } = useAuth0();
   const navigate = useNavigate();
-  const { isAuthenticated, loginWithRedirect } = useAuth0();
+  const { isAuthenticated, loginWithRedirect, isLoading } = useAuth0();
 
   const handleStart = () => {
     if (isAuthenticated) {
@@ -23,12 +22,19 @@ export const Landing = () => {
 
   return (
     <div>
-      <Typography>Landing page</Typography>
-      <Button variant="contained" onClick={handleStart}>
-        Get Started!!
-      </Button>
-      <LogoutButton />
-      <p>{isAuthenticated ? "login!" : "logout"}</p>
+      <Grid container direction="column" alignItems="center" justifyContent="center" height="100vh">
+        <Grid item md={6}>
+          <Typography variant="h2">Dev Meetup</Typography>
+        </Grid>
+        <Grid item md={6}>
+          <Button variant="contained" onClick={handleStart}>
+          {isAuthenticated ? "開く" : "ログイン"}
+          </Button>
+        </Grid>
+        <Grid item md={6}>
+          <GoodTeam />
+        </Grid>
+      </Grid>
     </div>
   );
 };
