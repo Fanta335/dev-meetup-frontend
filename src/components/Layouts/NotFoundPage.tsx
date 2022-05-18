db503a8a34816cdec68c14e18ae35ba0117c1672
Fanta335 <fantayusuke5210@gmail.com>
2022-05-18 15:20:40 +0900

    Add not found page

---
+++ src/components/Layouts/NotFoundPage.tsx
@@ -0,0 +1,13 @@
+import { Grid, Typography } from "@mui/material";
+import { ReactComponent as NotFound } from "../../assets/page_not_found.svg";
+
+export const NotFoundPage = () => {
+  return (
+    <>
+      <Grid container direction='column' justifyContent="center" alignItems='center' height='100vh'>
+        <Typography variant="h4">ページが見つかりませんでした。</Typography>
+        <NotFound width='50%' />
+      </Grid>
+    </>
+  );
+};
