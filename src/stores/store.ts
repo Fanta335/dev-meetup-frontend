bbd517a91e07ff748c488c8f54353c012712a1aa
Fanta335 <fantayusuke5210@gmail.com>
2022-05-21 17:57:35 +0900

    Add AsyncThunkConfig

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,5 +1,5 @@
 import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
-import { messageMiddleware } from "../features/message/messageMiddleware";
+import { chatMiddleware } from "../features/message/chatMiddleware";
 import { messageReducer } from "../features/message/messageSlice";
 import { roomReducer } from "../features/room/roomSlice";
 import { userReducer } from "../features/user/userSlice";
@@ -8,13 +8,18 @@ export const store = configureStore({
   reducer: {
     user: userReducer,
     room: roomReducer,
-    message: messageReducer
+    message: messageReducer,
   },
   middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware().concat([messageMiddleware])
-  }
+    return getDefaultMiddleware().concat([chatMiddleware]);
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppTunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, Action<string>>;
 export type AppDispatch = typeof store.dispatch;
+export type AsyncThunkConfig<T = unknown> = {
+  state: RootState;
+  dispatch: AppDispatch;
+  rejectValue: T;
+}
