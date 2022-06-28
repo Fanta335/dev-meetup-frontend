2d981eccb6ff767217b7524793bc5af30513e75d
Fanta335 <fantayusuke5210@gmail.com>
2022-06-28 12:54:33 +0900

    Add simple user profile layout

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,68 +1,39 @@
-import { IdToken, useAuth0 } from "@auth0/auth0-react";
-// import axios from "axios";
-import { useEffect, useState, VFC } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useEffect, VFC } from "react";
 import { Loading } from "./Loading";
-import { MysqlUser } from "../types";
+import { fetchUserProfile, selectCurrentUser } from "../features/user/userSlice";
+import { useAppDispatch, useAppSelector } from "../stores/hooks";
+import { Avatar, Button } from "@mui/material";
 
-const Profile: VFC = () => {
-  const { user, isAuthenticated, isLoading, getIdTokenClaims } = useAuth0();
-  const [mysqlUser, setMysqlUser] = useState<MysqlUser>();
-  // const [responseData, setResponseData] = useState<any>();
-  // const serverUrl = process.env.REACT_APP_API_URL || "http://localhost:3000";
+export const Profile: VFC = () => {
+  const { isAuthenticated, isLoading, getAccessTokenSilently } = useAuth0();
+  const dispatch = useAppDispatch();
+  const currentUser = useAppSelector(selectCurrentUser);
 
   useEffect(() => {
-    // const getUserData = async () => {
-    //   try {
-    //     const token = await getAccessTokenSilently();
-    //     console.log(token);
-    //     console.log(user?.sub);
-
-    //     const response = await axios(`${serverUrl}/users/`, {
-    //       headers: {
-    //         Authorization: `Bearer ${token}`,
-    //       },
-    //     });
-
-    //     const responseData = response.data;
-    //     console.log(response.data);
-    //     setResponseData(responseData);
-    //   } catch (e) {
-    //     console.log(e.message);
-    //   }
-    // };
-
-    const getUserMetaData = async () => {
-      try {
-        const claims = await getIdTokenClaims() as IdToken;
-        const namespace = process.env.REACT_APP_API_NAMESPACE;
-        const userMetaData = claims[namespace + '/mysqlUser'];
-        console.log(userMetaData);
-        setMysqlUser(userMetaData);
-      } catch (e) {
-        console.log(e);
-      }
+    const getUserProfile = async () => {
+      const token = await getAccessTokenSilently();
+      await dispatch(fetchUserProfile({ token }));
     };
 
-    getUserMetaData();
-  }, [getIdTokenClaims]);
+    getUserProfile();
+  }, [getAccessTokenSilently, dispatch]);
+  console.log("current user: ", currentUser);
 
   if (isLoading) {
     return <Loading />;
   }
 
-  console.log('this is profile.');
+  console.log("this is profile.");
 
   return isAuthenticated ? (
-    <div>
-      <img src={user?.picture} alt={user?.name} />
-      <h2>{user?.name}</h2>
-      <p>{user?.email}</p>
-      <h3>MySQL User Data</h3>
-      {mysqlUser ? <pre>{JSON.stringify(mysqlUser, null, 2)}</pre> : "No user data. "}
-    </div>
+    <>
+      <Avatar src={currentUser.avatar.url} sx={{ width: "100px", height: "100px" }} />
+      <p>id: {currentUser.id}</p>
+      <p>name: {currentUser.name}</p>
+      <Button variant="contained">プロフィールを編集</Button>
+    </>
   ) : (
     <h2>Not authenticated!</h2>
   );
 };
-
-export default Profile;
