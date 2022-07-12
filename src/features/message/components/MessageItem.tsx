6eada84c63856cb6c8c9f77f1ae9e4bfba7b037a
Fanta335 <fantayusuke5210@gmail.com>
2022-07-12 22:00:13 +0900

    Change to display only when mouse hovered

--- src/features/message/components/MessageItem.tsx
+++ src/features/message/components/MessageItem.tsx
@@ -1,5 +1,5 @@
 import { Avatar, Box, Typography } from "@mui/material";
-import { memo, VFC } from "react";
+import { memo, useState, VFC } from "react";
 import dayjs from "dayjs";
 import { useAppSelector } from "../../../stores/hooks";
 import { selectCurrentUsers } from "../../user/userSlice";
@@ -14,6 +14,8 @@ type Props = {
 };
 
 export const MessageItem: VFC<Props> = memo(({ messageId }) => {
+  const [display, setDisplay] = useState(false);
+
   const currentUsers = useAppSelector(selectCurrentUsers);
 
   const message = useAppSelector((state) => selectMessageById(state, messageId));
@@ -25,24 +27,33 @@ export const MessageItem: VFC<Props> = memo(({ messageId }) => {
   return (
     <>
       {/* If this message is a reply, append reply accessory. */}
-      <Box>{parentMessageId && <ReplyAccessory parentMessageId={parentMessageId} />}</Box>
-      <Box component="div" sx={[{ display: "flex", pb: 3, px: 1, bgcolor: "#ffa" }, { "&:hover": { bgcolor: "#eee" } }]}>
+      <Box sx={[{ px: 1 }, { "&:hover": { bgcolor: "#00000013" } }]}>{parentMessageId && <ReplyAccessory parentMessageId={parentMessageId} />}</Box>
+      <Box
+        component="div"
+        sx={[{ display: "flex", pb: 3, px: 1 }, { "&:hover": { bgcolor: "#00000013" } }]}
+        onMouseEnter={() => {
+          setDisplay(true);
+        }}
+        onMouseLeave={() => setDisplay(false)}
+      >
         <Box>
           <Avatar alt={author ? author.name : "removed-user"} sx={{ mr: 1 }} src={author?.avatar ? author.avatar.url : ""} />
         </Box>
         <Box sx={{ flexGrow: 1, display: "flex", flexDirection: "column", ml: 1 }}>
-          <Box sx={{ display: "flex", alignItems: "baseline" }}>
-            {author ? (
-              <Typography variant="body1">{author.name}</Typography>
-            ) : (
-              <Typography variant="body2" sx={{ fontStyle: "italic" }}>
-                脱退したユーザー
+          <Box sx={{ display: "flex", alignItems: "center", height: "50px" }}>
+            <Box sx={{ flexGrow: 1, display: "flex", alignItems: "center" }}>
+              {author ? (
+                <Typography variant="body1">{author.name}</Typography>
+              ) : (
+                <Typography variant="body2" sx={{ fontStyle: "italic" }}>
+                  脱退したユーザー
+                </Typography>
+              )}
+              <Typography variant="body2" color="text.secondary" sx={{ ml: 1 }}>
+                {formattedDate}
               </Typography>
-            )}
-            <Typography variant="body2" color="text.secondary" sx={{ ml: 1 }}>
-              {formattedDate}
-            </Typography>
-            <MessageMenu messageId={message.id} />
+            </Box>
+            <Box>{display && <MessageMenu messageId={message.id} />}</Box>
           </Box>
           <Box>
             <MessageContent message={message} />
