71d9c592c5ad1e2d89bbd2d02edfcfbc64db8733
Fanta335 <fantayusuke5210@gmail.com>
2022-07-13 16:07:44 +0900

    Improve layout so that scrollbars are visible

--- src/components/Layouts/Navbar.tsx
+++ src/components/Layouts/Navbar.tsx
@@ -20,9 +20,9 @@ export const Navbar = () => {
   const settings = ["Profile", "Account", "Logout"];
 
   return (
-    <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+    <AppBar position="fixed" sx={{ mr: "12px", zIndex: (theme) => theme.zIndex.drawer + 1 }}>
       <Toolbar>
-        <Box sx={{ flexGrow: 1, pl: "50px", display: "flex", alignItems: "center" }}>
+        <Box sx={{ flexGrow: 1, pl: "62px", display: "flex", alignItems: "center" }}>
           <LocationName location={location} />
           {location === "room" && loading === "idle" && <RoomSettingsMenu />}
         </Box>
