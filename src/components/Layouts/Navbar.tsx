f652c207e4be1ca26b565255aba5ffe811f83a0f
Fanta335 <fantayusuke5210@gmail.com>
2022-07-14 16:25:53 +0900

    Remove UserSettingsButton

--- src/components/Layouts/Navbar.tsx
+++ src/components/Layouts/Navbar.tsx
@@ -1,9 +1,7 @@
 import { AppBar, Box, Toolbar } from "@mui/material";
-import { useState } from "react";
 import { RoomSettingsMenu } from "../../features/room/components/RoomSettingsMenu";
 import { selectCurrentRoomLoading, selectLocation } from "../../features/room/roomSlice";
 import { ToggleRoomMemberDrawerButton } from "../../features/user/components/ToggleRoomMemberDrawerButton";
-import { UserSettingsButton } from "../../features/user/components/UserSettingsButton";
 import { useAppSelector } from "../../stores/hooks";
 import LogoutButton from "../auth/LogoutButton";
 import { LocationName } from "./LocationName";
@@ -11,15 +9,6 @@ import { LocationName } from "./LocationName";
 export const Navbar = () => {
   const location = useAppSelector(selectLocation);
   const loading = useAppSelector(selectCurrentRoomLoading);
-  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
-  };
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-  const settings = ["Profile", "Account", "Logout"];
-  // const isInRoom = location === "room" && loading === "idle";
 
   return (
     <AppBar position="fixed" sx={{ mr: "12px", zIndex: (theme) => theme.zIndex.drawer + 1 }}>
@@ -30,14 +19,6 @@ export const Navbar = () => {
         </Box>
         <LogoutButton />
         {location === "room" && <ToggleRoomMemberDrawerButton />}
-        <Box sx={{ flexGrow: 0 }}>
-          <UserSettingsButton
-            handleOpenUserMenu={handleOpenUserMenu}
-            handleCloseUserMenu={handleCloseUserMenu}
-            anchorElUser={anchorElUser}
-            settings={settings}
-          />
-        </Box>
       </Toolbar>
     </AppBar>
   );
