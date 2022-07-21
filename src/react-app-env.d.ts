96f1a9889922441c3b789adf26a000d48ab13b4e
Fanta335 <fantayusuke5210@gmail.com>
2022-07-21 18:12:34 +0900

    Add client app url to .env file

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,11 +1,12 @@
 /// <reference types="react-scripts" />
 namespace NodeJS {
   interface ProcessEnv {
-    PORT: number,
-    REACT_APP_AUTH0_DOMAIN: string,
-    REACT_APP_AUTH0_CLIENT_ID: string,
-    REACT_APP_AUTH0_AUDIENCE: string,
-    REACT_APP_API_URL: string,
-    REACT_APP_API_NAMESPACE: string
+    PORT: number;
+    REACT_APP_AUTH0_DOMAIN: string;
+    REACT_APP_AUTH0_CLIENT_ID: string;
+    REACT_APP_AUTH0_AUDIENCE: string;
+    REACT_APP_API_URL: string;
+    REACT_APP_API_NAMESPACE: string;
+    REACT_APP_CLIENT_URL: string;
   }
 }
