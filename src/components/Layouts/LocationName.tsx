f6200ccbd75d273f6a5a73ab985ac6efe0f9930c
Fanta335 <fantayusuke5210@gmail.com>
2022-07-27 20:12:33 +0900

    Add lock icon if the room is private

--- src/components/Layouts/LocationName.tsx
+++ src/components/Layouts/LocationName.tsx
@@ -1,8 +1,9 @@
-import { Typography } from "@mui/material";
+import { Tooltip, Typography, Zoom } from "@mui/material";
 import { VFC } from "react";
 import { selectCurrentRoom } from "../../features/room/roomSlice";
 import { Location } from "../../features/room/types";
 import { useAppSelector } from "../../stores/hooks";
+import LockIcon from "@mui/icons-material/Lock";
 
 type Prop = {
   location: Location;
@@ -17,6 +18,11 @@ export const LocationName: VFC<Prop> = ({ location }) => {
         <Typography variant="h6" noWrap sx={{ textDecoration: "none", color: "inherit" }}>
           {currentRoom.entity.name}
         </Typography>
+      )}
+      {location === "room" && currentRoom.entity.isPrivate && (
+        <Tooltip title="非公開" placement="bottom" arrow TransitionComponent={Zoom}>
+          <LockIcon sx={{ ml: 2 }} />
+        </Tooltip>
       )}
       {location === "profile" && (
         <Typography variant="h6" noWrap sx={{ textDecoration: "none", color: "inherit" }}>
