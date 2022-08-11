7a0e0766fb647ae8cd151cbb9441cc4bc6b1971f
Fanta335 <fantayusuke5210@gmail.com>
2022-08-11 17:00:23 +0900

    Change to use `useSearchParams()` to retrieve url search params

--- src/features/room/components/SearchRoomResultPagination.tsx
+++ src/features/room/components/SearchRoomResultPagination.tsx
@@ -1,20 +1,19 @@
 import { Pagination, PaginationItem } from "@mui/material";
 import { VFC } from "react";
-import { Link } from "react-router-dom";
-import { useLocation } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 type Props = {
   count: number;
 };
 
 export const SearchRoomResultPagination: VFC<Props> = ({ count }) => {
-  const location = useLocation();
-  const queryParams = new URLSearchParams(location.search);
-  const query = queryParams.get("query") || "";
-  const offset = Number(queryParams.get("offset")) || 0;
-  const limit = Number(queryParams.get("limit")) || 6;
-  const sort = queryParams.get("sort") || "date";
-  const order = queryParams.get("order") || "a";
+  const [searchParams] = useSearchParams();
+  const query = searchParams.get("query") || "";
+  const offset = Number(searchParams.get("offset")) || 0;
+  const limit = Number(searchParams.get("limit")) || 6;
+  const sort = searchParams.get("sort") || "date";
+  const order = searchParams.get("order") || "a";
+  const tagId = searchParams.get("tagId") || "";
 
   return (
     <>
@@ -24,7 +23,7 @@ export const SearchRoomResultPagination: VFC<Props> = ({ count }) => {
         renderItem={(item) => (
           <PaginationItem
             component={Link}
-            to={`?query=${query}&offset=${item.page ? (item.page - 1) * limit : 0}&limit=${limit}&sort=${sort}&order=${order}`}
+            to={`?query=${query}&offset=${item.page ? (item.page - 1) * limit : 0}&limit=${limit}&sort=${sort}&order=${order}&tagId=${tagId}`}
             {...item}
           />
         )}
