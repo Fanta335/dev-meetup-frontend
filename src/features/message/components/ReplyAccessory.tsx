d46e87263bb3eb016e458b99d230a545d39cfed9
Fanta335 <fantayusuke5210@gmail.com>
2022-08-26 17:25:12 +0900

    Change `ReplyAccessory` to fetch parent message data

--- src/features/message/components/ReplyAccessory.tsx
+++ src/features/message/components/ReplyAccessory.tsx
@@ -1,11 +1,12 @@
-import { VFC } from "react";
+import { useEffect, VFC } from "react";
 import ShortcutIcon from "@mui/icons-material/Shortcut";
-import { useAppSelector } from "../../../stores/hooks";
-import { selectCurrentMessages } from "../messageSlice";
+import { useAppDispatch, useAppSelector } from "../../../stores/hooks";
+import { fetchOneMessage, selectCurrentMessages } from "../messageSlice";
 import { Message } from "../types";
 import { Box } from "@mui/material";
 import { ParentMessageItem } from "./ParentMessageItem";
 import { DeletedParentMessageItem } from "./DeletedParentMessageItem";
+import { useAuth0 } from "@auth0/auth0-react";
 
 type Props = {
   parentMessageId: number;
@@ -15,6 +16,16 @@ type Props = {
 export const ReplyAccessory: VFC<Props> = ({ parentMessageId, handleClickReply }) => {
   const currentMessages = useAppSelector(selectCurrentMessages);
   const parentMessage: Message | undefined = currentMessages.byIds[parentMessageId];
+  const dispatch = useAppDispatch();
+  const { getAccessTokenSilently } = useAuth0();
+
+  useEffect(() => {
+    const fetchParentMessage = async () => {
+      const token = await getAccessTokenSilently();
+      await dispatch(fetchOneMessage({ token, messageId: parentMessageId }));
+    };
+    fetchParentMessage();
+  }, [dispatch, getAccessTokenSilently, parentMessageId]);
 
   return (
     <Box sx={{ display: "flex", alignItems: "center", px: 3 }}>
