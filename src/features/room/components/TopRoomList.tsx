aef428ef739286e47bae430f3348744bcd3c7f79
Fanta335 <fantayusuke5210@gmail.com>
2022-08-27 14:40:55 +0900

    Fix layout

--- src/features/room/components/TopRoomList.tsx
+++ src/features/room/components/TopRoomList.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Grid, Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 import { useAppSelector } from "../../../stores/hooks";
 import { selectSearchedrooms } from "../roomSlice";
 import { SmallRoomCard } from "./SmallRoomCard";
@@ -7,22 +7,20 @@ export const TopRoomList = () => {
   const searchedRooms = useAppSelector(selectSearchedrooms);
   return (
     <>
-      <Box sx={{ display: "flex", flexDirection: "column", my: 3, mx: 4 }}>
-        <Container>
-          <Typography variant="h6" sx={{ color: "white", ml: 1 }}>
-            最近作られた部屋
-          </Typography>
-          <Grid container>
-            {searchedRooms.allIds.map((id) => {
-              const room = searchedRooms.byIds[id];
-              return (
-                <Grid key={id} item xs={6} md={4} lg={3}>
-                  <SmallRoomCard room={room} />
-                </Grid>
-              );
-            })}
-          </Grid>
-        </Container>
+      <Box sx={{ display: "flex", flexDirection: "column", my: 3, mx: 4, width: "100%" }}>
+        <Typography variant="h6" sx={{ color: "white", ml: 1 }}>
+          最近作られた部屋
+        </Typography>
+        <Grid container>
+          {searchedRooms.allIds.map((id) => {
+            const room = searchedRooms.byIds[id];
+            return (
+              <Grid key={id} item xs={6} md={4} lg={3}>
+                <SmallRoomCard room={room} />
+              </Grid>
+            );
+          })}
+        </Grid>
       </Box>
     </>
   );
