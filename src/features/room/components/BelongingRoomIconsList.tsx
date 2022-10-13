c08b25410d0c3d672e9ba8991d7b596dfb838988
Fanta335 <fantayusuke5210@gmail.com>
2022-10-13 19:17:18 +0900

    Improve side bar

--- src/features/room/components/BelongingRoomIconsList.tsx
+++ src/features/room/components/BelongingRoomIconsList.tsx
@@ -2,17 +2,19 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { Avatar, IconButton, List, ListItem, Tooltip, Typography, Zoom } from "@mui/material";
 import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../../stores/hooks";
-import { fetchBelongingRooms, fetchOwnRooms, selectBelongingRooms } from "../roomSlice";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import { fetchBelongingRooms, fetchOwnRooms, selectBelongingRooms, selectCurrentRoom, selectLocation } from "../roomSlice";
 import { Link } from "react-router-dom";
 import { Auth0User } from "../../auth/types";
 import { getCurrentUser } from "../../user/utils/getCurrentUser";
+import ChairIcon from "@mui/icons-material/Chair";
 
 export const BelongingRoomIconsList = () => {
   const { getAccessTokenSilently, user } = useAuth0<Auth0User>();
   const dispatch = useAppDispatch();
   const belongingRooms = useAppSelector(selectBelongingRooms);
   const currentUser = getCurrentUser(user);
+  const currentRoom = useAppSelector(selectCurrentRoom);
+  const location = useAppSelector(selectLocation);
 
   useEffect(() => {
     const fetchMyRooms = async () => {
@@ -40,12 +42,19 @@ export const BelongingRoomIconsList = () => {
             arrow
             TransitionComponent={Zoom}
           >
-            <ListItem button sx={{ display: "flex", justifyContent: "center", height: "50px" }}>
-              <IconButton aria-label={belongingRooms.byIds[roomId].name} component={Link} to={`rooms/${roomId}`}>
+            <ListItem button sx={{ display: "flex", justifyContent: "center", height: "60px" }}>
+              <IconButton
+                aria-label={belongingRooms.byIds[roomId].name}
+                component={Link}
+                to={`rooms/${roomId}`}
+                sx={location === "room" && roomId === currentRoom.entity.id ? { bgcolor: "#ffffff" } : {}}
+              >
                 {belongingRooms.byIds[roomId].avatar ? (
-                  <Avatar src={belongingRooms.byIds[roomId].avatar.url} sx={{ height: "45px", width: "45px" }} />
+                  <Avatar src={belongingRooms.byIds[roomId].avatar?.url} sx={{ height: "45px", width: "45px" }} />
                 ) : (
-                  <AccountCircleIcon sx={{ fontSize: "54px" }} color="secondary" />
+                  <Avatar sx={{ height: "45px", width: "45px" }}>
+                    <ChairIcon />
+                  </Avatar>
                 )}
               </IconButton>
             </ListItem>
