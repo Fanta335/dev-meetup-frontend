53c1fc693f132093a8d8aab455332b59b8ab4684
Fanta335 <fantayusuke5210@gmail.com>
2022-10-18 16:28:26 +0900

    Fix layout

--- src/components/Layouts/ForbiddenPage.tsx
+++ src/components/Layouts/ForbiddenPage.tsx
@@ -1,15 +1,20 @@
-import { Grid, Typography } from '@mui/material';
-import {ReactComponent as QuestionMark} from '../../assets/images/question.svg';
-
+import { Grid, Typography } from "@mui/material";
+import { ReactComponent as QuestionMark } from "../../assets/images/question.svg";
 
 export const ForbiddenPage = () => {
   return (
     <>
-    <Grid container direction='column' alignItems='center' justifyContent='center' height='80%'>
-      <QuestionMark />
-      <Typography variant='h5' fontWeight='bold'>部屋がありません。</Typography>
-      <Typography>部屋に入るための権限がない、あるいは部屋そのものが存在しないかのどちらかでしょう。</Typography>
-    </Grid>
+      <Grid container direction="column" alignItems="center" justifyContent="center" height="80%" spacing={4}>
+        <Grid item>
+          <QuestionMark />
+        </Grid>
+        <Grid container item justifyContent="center">
+          <Typography variant="h5" fontWeight="bold">
+            部屋がありません。
+          </Typography>
+          <Typography variant="subtitle1">部屋に入るための権限がない、あるいは部屋そのものが存在しないかのどちらかでしょう。</Typography>
+        </Grid>
+      </Grid>
     </>
-  )
-}
+  );
+};
