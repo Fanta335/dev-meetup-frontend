ac1e0248f620e4ffd230de12e065c4e2757c6fe9
Fanta335 <fantayusuke5210@gmail.com>
2022-10-20 18:32:01 +0900

    Improve `MessageItem` to be highlighted while being replyed

--- src/features/message/components/MessageItem.tsx
+++ src/features/message/components/MessageItem.tsx
@@ -3,7 +3,7 @@ import { FC, memo, useEffect, useState } from "react";
 import dayjs from "dayjs";
 import { useAppDispatch, useAppSelector } from "../../../stores/hooks";
 import { selectCurrentUsers } from "../../user/userSlice";
-import { messageActions, selectMessageById } from "../messageSlice";
+import { messageActions, selectMessageById, selectMessageReply } from "../messageSlice";
 import { MessageMenu } from "./MessageMenu";
 import { MessageContent } from "./MessageContent";
 import { ReplyAccessory } from "./ReplyAccessory";
@@ -23,6 +23,10 @@ export const MessageItem: FC<Props> = memo(({ messageId, virtualListId, handleCl
   const message = useAppSelector((state) => selectMessageById(state, messageId));
   const parentMessageId = message?.parentId;
   const author = currentUsers.members.byIds[message?.authorId.toString()] as User | undefined;
+
+  const messageReply = useAppSelector(selectMessageReply);
+  const isReplyingToThisMessage = messageId === messageReply.parentMessageId;
+
   const formattedDate = dayjs(message?.createdAt).format("YYYY/MM/DD HH:mm");
 
   const dispatch = useAppDispatch();
@@ -36,34 +40,36 @@ export const MessageItem: FC<Props> = memo(({ messageId, virtualListId, handleCl
       <Box sx={[{ px: 1 }, { "&:hover": { bgcolor: "#00000013" } }]}>
         {parentMessageId && <ReplyAccessory parentMessageId={parentMessageId} handleClickReply={handleClickReply} />}
       </Box>
-      <Box
-        component="div"
-        sx={[{ display: "flex", pb: 3, px: 1 }, { "&:hover": { bgcolor: "#00000013" } }]}
-        onMouseEnter={() => {
-          setDisplay(true);
-        }}
-        onMouseLeave={() => setDisplay(false)}
-      >
-        <Box>
-          <Avatar alt={author ? author.name : "removed-user"} sx={{ mr: 1, mt: 1 }} src={author ? author.avatar.url : ""} />
-        </Box>
-        <Box sx={{ flexGrow: 1, display: "flex", flexDirection: "column", ml: 1 }}>
-          <Box sx={{ display: "flex", alignItems: "center", height: "50px" }}>
-            <Box sx={{ flexGrow: 1, display: "flex", alignItems: "center" }}>
-              {author ? (
-                <Typography variant="body1">{author.name}</Typography>
-              ) : (
-                <Typography variant="body2" sx={{ fontStyle: "italic" }}>
-                  脱退したユーザー
-                </Typography>
-              )}
-              <Typography variant="body2" color="text.secondary" sx={{ ml: 1 }}>
-                {formattedDate}
-              </Typography>
-            </Box>
-            <Box>{display && message && <MessageMenu messageId={message.id} />}</Box>
+      <Box sx={isReplyingToThisMessage ? { bgcolor: "#1100ff2f", borderLeft: "5px solid #772CE8" } : {}}>
+        <Box
+          component="div"
+          sx={[{ display: "flex", pb: 3, px: 1 }, { "&:hover": { bgcolor: "#00000013" } }]}
+          onMouseEnter={() => {
+            setDisplay(true);
+          }}
+          onMouseLeave={() => setDisplay(false)}
+        >
+          <Box>
+            <Avatar alt={author ? author.name : "removed-user"} sx={{ mr: 1, mt: 1 }} src={author ? author.avatar.url : ""} />
           </Box>
-          <Box>{message && <MessageContent message={message} />}</Box>
+          <Box sx={{ flexGrow: 1, display: "flex", flexDirection: "column", ml: 1 }}>
+            <Box sx={{ display: "flex", alignItems: "center", height: "50px" }}>
+              <Box sx={{ flexGrow: 1, display: "flex", alignItems: "center" }}>
+                {author ? (
+                  <Typography variant="body1">{author.name}</Typography>
+                ) : (
+                  <Typography variant="body2" sx={{ fontStyle: "italic" }}>
+                    脱退したユーザー
+                  </Typography>
+                )}
+                <Typography variant="body2" color="text.secondary" sx={{ ml: 1 }}>
+                  {formattedDate}
+                </Typography>
+              </Box>
+              <Box>{display && message && <MessageMenu messageId={message.id} />}</Box>
+            </Box>
+            <Box>{message && <MessageContent message={message} />}</Box>
+          </Box>
         </Box>
       </Box>
     </>
